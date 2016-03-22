8715c5834dd3390e5a9574d095c1e3e8fa64ccb1
David Silva <david.silva@digital.cabinet-office.gov.uk>
2016-03-22 19:35:20 +0000

    Change variable name from rows to RowWraper
    For better readability

--- app/filter-helpers.js
+++ app/filter-helpers.js
@@ -21,7 +21,7 @@ var sectionFilter = function (allContent, pageSection) {
 var rowsWithThreeColumns = function(allContent, pageSection) {
   var filteredList = _filteredList(allContent, pageSection);
 
-  var rows    = { rows: [] };
+  var rowWrapper  = { rows: [] };
   var columns = [];
 
   var firstSixElements = filteredList.slice(0, 5);
@@ -35,7 +35,7 @@ var rowsWithThreeColumns = function(allContent, pageSection) {
     if ((columns.length > 0) && (columns.length % 3 == 0)) {
       // when the columns array has reached 3 elements
       // then add those columns to the rows
-      rows.rows.push({columns: columns});
+      rowWrapper.rows.push({columns: columns});
 
       // Then clear the columns array
       // and push the current column to the columns array
@@ -47,9 +47,9 @@ var rowsWithThreeColumns = function(allContent, pageSection) {
   }
 
   // push the last columns to the last row
-  rows.rows.push({columns: columns});
+  rowWrapper.rows.push({columns: columns});
 
-  return rows;
+  return rowWrapper;
 }
 
 var _filteredList = function(allContent, pageSection) {
