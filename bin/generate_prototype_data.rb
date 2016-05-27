81b6db59c17469f5b0a54cde98220ce7672f3870
Mo Baig <mo@mobaig.com>
2016-05-27 15:04:27 +0100

    Add some rudimentary error handling to GeneratePrototypeData

--- bin/generate_prototype_data.rb
+++ bin/generate_prototype_data.rb
@@ -9,40 +9,61 @@ require 'yaml'
 class GeneratePrototypeData
   def run
     dataset = {}
+    errors  = {}
+
     taxon_slugs.each do |taxon_slug|
-      puts "*** FETCH START - #{taxon_slug} ***"
-      puts "> Getting taxon document from content store..."
-      taxon_document = taxon_document_for(taxon_slug)
+      with_error_handling(errors, taxon_slug) do
+        puts "*** Start... #{taxon_slug} ***"
+        puts "> Getting taxon document from content store..."
+        taxon_document = taxon_document_for(taxon_slug)
 
-      puts "> Getting the child taxons..."
-      child_taxons = child_taxons_of(taxon_slug)
+        puts "> Getting the child taxons..."
+        child_taxons = child_taxons_of(taxon_slug)
 
-      puts "> Getting tagged content items..."
-      content_items = content_items_tagged_to(taxon_slug)
+        puts "> Getting tagged content items..."
+        content_items = content_items_tagged_to(taxon_slug)
 
-      puts "> Getting timestamps, format, and display_type for each content item..."
-      content_items.each do |content_item|
-        puts "--> Fetching updated_at & public_updated_at for #{content_item["base_path"]}"
-        add_timestamps_to(content_item)
+        puts "> Getting timestamps, format, and display_type for each content item..."
+        content_items.each do |content_item|
+          puts "--> Fetching updated_at & public_updated_at for #{content_item["base_path"]}"
+          add_timestamps_to(content_item)
 
-        puts "--> Fetching search data for #{content_item["base_path"]}"
-        add_format_info_to(content_item)
+          puts "--> Fetching search data for #{content_item["base_path"]}"
+          add_format_info_to(content_item)
+        end
+        puts "*** END #{taxon_slug} ***"
+        puts
+
+        dataset[taxon_slug] = {
+          base_path:       taxon_document["base_path"],
+          title:           taxon_document["title"],
+          parent:          taxon_document["links"]["parent"],
+          children:        child_taxons,
+          tagged_content:  content_items,
+        }
       end
-      puts "*** FETCH END - #{taxon_slug} ***"
-      puts
-
-      dataset[taxon_slug] = {
-        base_path:       taxon_document["base_path"],
-        title:           taxon_document["title"],
-        parent:          taxon_document["links"]["parent"],
-        children:        child_taxons,
-        tagged_content:  content_items,
-      }
     end
 
     writable_data = JSON.dump dataset
     File.open("#{File.dirname(__FILE__)}/../app/data/taxonomy-data.json", 'wb') do |file|
       file.write(writable_data)
+    end
+
+    puts
+    puts "Finished. Error summary:"
+    puts "🙌  None reported ️️🙌 " if errors.empty?
+    errors.each do |taxon_slug, error|
+      puts "#{taxon_slug}: #{error}"
+    end
+  end
+
+  def with_error_handling(errors, taxon_slug)
+    begin
+      yield
+    rescue => e
+      puts ">...something broke, continuing..."
+      puts
+      errors[taxon_slug] = e.inspect
     end
   end
 
