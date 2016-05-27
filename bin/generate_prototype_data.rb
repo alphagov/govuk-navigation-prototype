da4c5468281ce429f75f5c2bb7321b95f2b6f50a
Mo Baig <mo@mobaig.com>
2016-05-27 14:21:35 +0100

    Clean up the structure of the GeneratePrototypeData scripts
    - Extract several methods to make #run easier to read.
    - Extract the hardcoded list of taxon slugs into a YAML file.

--- bin/generate_prototype_data.rb
+++ bin/generate_prototype_data.rb
@@ -4,55 +4,39 @@ require 'pry'
 require 'net/http'
 require 'json'
 require 'date'
+require 'yaml'
 
 class GeneratePrototypeData
-  # TODO: add error handling (particularly network errors)
-
   def run
     dataset = {}
-    taxons.each do |taxon_slug|
+    taxon_slugs.each do |taxon_slug|
       puts "*** FETCH START - #{taxon_slug} ***"
+      puts "> Getting taxon document from content store..."
+      taxon_document = taxon_document_for(taxon_slug)
 
-      puts "Getting the parent taxon..."
-      taxon_endpoint = URI "#{hostname}/api/content/alpha-taxonomy/#{taxon_slug}"
-      taxon_document = JSON.parse(Net::HTTP.get taxon_endpoint)
-      parent_document = taxon_document["links"]["parent"]
+      puts "> Getting the child taxons..."
+      child_taxons = child_taxons_of(taxon_slug)
 
-      puts "Getting the child taxons..."
-      children_endpoint = URI "#{hostname}/api/incoming-links/alpha-taxonomy/#{taxon_slug}?types[]=parent&#{DateTime.now}"
-      children_documents = JSON.parse(Net::HTTP.get children_endpoint)["parent"]
+      puts "> Getting tagged content items..."
+      content_items = content_items_tagged_to(taxon_slug)
 
-      puts "Getting tagged content items..."
-      content_endpoint = URI "#{hostname}/api/incoming-links/alpha-taxonomy/#{taxon_slug}?types[]=alpha_taxons"
-      content_items    = JSON.parse(Net::HTTP.get content_endpoint)["alpha_taxons"]
-
-      puts "Getting format and display_type for each content item..."
+      puts "> Getting timestamps, format, and display_type for each content item..."
       content_items.each do |content_item|
+        puts "--> Fetching updated_at & public_updated_at for #{content_item["base_path"]}"
+        add_timestamps_to(content_item)
 
-        puts "-- Fetching updated_at & public_updated_at for #{content_item["base_path"]}"
-        content_item_endpoint = JSON.parse(Net::HTTP.get(URI(content_item["api_url"])))
-        content_item["updated_at"]        = content_item_endpoint["updated_at"]
-        content_item["public_updated_at"] = content_item_endpoint["public_updated_at"]
-
-        puts "-- Fetching search data for #{content_item["base_path"]}"
-        search_endpoint = URI "#{hostname}/api/search.json?filter_link=#{content_item["base_path"]}"
-        search_result = JSON.parse(Net::HTTP.get search_endpoint)
-
-        if search_result["results"].first
-          content_item["display_type"] = search_result["results"].first["display_type"]
-          content_item["format"] = search_result["results"].first["format"]
-        end
+        puts "--> Fetching search data for #{content_item["base_path"]}"
+        add_format_info_to(content_item)
       end
-
       puts "*** FETCH END - #{taxon_slug} ***"
       puts
 
       dataset[taxon_slug] = {
         base_path:       taxon_document["base_path"],
         title:           taxon_document["title"],
-        parent:          parent_document,
-        children:        children_documents,
-        tagged_content:  content_items
+        parent:          taxon_document["links"]["parent"],
+        children:        child_taxons,
+        tagged_content:  content_items,
       }
     end
 
@@ -63,81 +47,41 @@ class GeneratePrototypeData
   end
 
   private
-  def taxons
-    @taxons ||= %w{
-      14-to-19-years
-      a-level
-      annual-tests-for-buses-and-coaches
-      annual-tests-for-lorrys-buses-and-trailers
-      approved-driving-instruction-adi
-      being-an-hgv-operator
-      being-an-mot-tester
-      booking-tests
-      booking-tests-and-checking-licences-for-your-pupils
-      bus-and-coach-accessibility-certification-forms
-      car-tax
-      cars
-      cars-and-trailers
-      childcare-and-early-years
-      childminders
-      children-s-centres
-      curriculum-and-qualifications
-      despatch-newsletter-for-driver-and-rider-trainers
-      drink-drive-rehabilitation
-      driving
-      driving-and-vehicle-businesses
-      driving-and-vehicles
-      driving-licenses
-      driving-lorries-buses-or-goods-vehicles
-      driving-test-policies-and-test-centre-rules
-      driving-vans-and-running-fleets-of-vans
-      driving-with-a-disability-or-health-condition
-      early-years-foundation-stage-eyfs
-      early-years-pupil-premium
-      early-years-settings
-      education
-      gcse
-      get-and-check-an-mot
-      good-practice
-      highway-code
-      improving-your-training-skills
-      individual-vehicle-approval-iva
-      inspections
-      key-stage-3
-      large-goods-vehicle-instruction
-      learning-to-drive-or-ride
-      lorries-and-buses
-      lorry-bus-or-trailer-tests-and-permits
-      moped-and-motorcycle-training
-      mot-and-vehicle-insurance
-      mot-forms
-      mot-manuals-and-notices
-      mot-test-service-modernisation
-      motorcycle-instruction
-      motorcycle-single-vehicle-approval-msva
-      motorcycles
-      national-curriculum
-      number-plates-and-vehicle-registration
-      nurseries
-      operating-buses-and-coaches
-      parking-including-blue-badge
-      phonics
-      primary
-      public-service-vehicle-licenses-psvs
-      running-a-local-bus-service
-      running-an-approved-tachograph-centre
-      running-an-mot-test-station
-      schools-and-colleges
-      special-educational-needs-send
-      specialist-vehicles
-      statistics-research-department-information
-      teaching-people-to-drive-or-ride
-      tests-and-licenses-for-lorries
-      training-for-early-years-staff
-      vehicle-recalls-and-safety-defects
-      vehicle-safety
-      vehicles
-    }
+  def child_taxons_of(taxon_slug)
+    children_endpoint = URI "#{hostname}/api/incoming-links/alpha-taxonomy/#{taxon_slug}?types[]=parent&#{DateTime.now}"
+    JSON.parse(Net::HTTP.get children_endpoint)["parent"]
+  end
+
+  def content_items_tagged_to(taxon_slug)
+    content_endpoint = URI "#{hostname}/api/incoming-links/alpha-taxonomy/#{taxon_slug}?types[]=alpha_taxons"
+    JSON.parse(Net::HTTP.get content_endpoint)["alpha_taxons"]
+  end
+
+  def add_timestamps_to(content_item)
+    content_item_endpoint = JSON.parse(Net::HTTP.get(URI(content_item["api_url"])))
+    content_item["updated_at"] = content_item_endpoint["updated_at"]
+    content_item["public_updated_at"] = content_item_endpoint["public_updated_at"]
+  end
+
+  def add_format_info_to(content_item)
+    search_endpoint = URI "#{hostname}/api/search.json?filter_link=#{content_item["base_path"]}"
+    search_result = JSON.parse(Net::HTTP.get search_endpoint)
+
+    if search_result["results"].first
+      content_item["display_type"] = search_result["results"].first["display_type"]
+      content_item["format"] = search_result["results"].first["format"]
+    end
+  end
+
+  def taxon_document_for(taxon_slug)
+    taxon_endpoint = URI "#{hostname}/api/content/alpha-taxonomy/#{taxon_slug}"
+    JSON.parse(Net::HTTP.get taxon_endpoint)
+  end
+
+  def taxon_slugs
+    @taxon_slugs ||= YAML.load_file(File.expand_path(
+      "../../app/data/taxon-slugs.yaml", __FILE__
+    ))["taxon_slugs"]
   end
 
   def hostname
